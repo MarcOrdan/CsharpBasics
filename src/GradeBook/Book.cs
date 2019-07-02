aa14cf06386a1b84249ef43a9520f030672fb05e
MarcOrdan <marcordan@gmail.com>
2019-07-02 17:49:34 +0800

    finish grade book console app

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace GradeBook
@@ -14,6 +15,25 @@ namespace GradeBook
     public void AddGrade(double grade)
     {
       grades.Add(grade);
+    }
+
+    public void showStatistics()
+    {
+            var result = 0.0;
+            var highestGrade = double.MinValue;
+            var lowestGrade = double.MaxValue;
+
+            foreach(var number in grades)
+            {
+                highestGrade = Math.Max(number, highestGrade);
+                lowestGrade = Math.Min(number, lowestGrade);
+                result += number;
+            }
+
+            result /= grades.Count;
+            Console.WriteLine($"The average grade is: {result:N1}");
+            Console.WriteLine($"The highest grade is: {highestGrade:N1}");
+            Console.WriteLine($"The lowest grade is: {lowestGrade:N1}");
     }
 
     private List<double> grades;
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -9,21 +9,10 @@ namespace GradeBook
         {
 
             var book = new Book("Mac Book");
-            book.AddGrade(1.5);
-
-            var grades = new List<double>() {1.5, 3.5, 7.5, 4.5};
-            grades.Add(2.5);
-
-            var result = 0.0;
-            foreach(var number in grades)
-            {
-                result += number;
-            }
-
-            result /= grades.Count;
-            Console.WriteLine($"The average grade is: {result:N1}");
-
-
+            book.AddGrade(50.5);
+            book.AddGrade(23.55);
+            book.AddGrade(7.7);
+            book.showStatistics();
         }
     }
 }
