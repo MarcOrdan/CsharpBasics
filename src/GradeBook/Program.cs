e14c30736d1e4ac9ab6b94cda2d1f9a4a22dde41
MarcOrdan <marcordan@gmail.com>
2019-07-02 16:22:43 +0800

    adding method in book class

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GradeBook
 {
@@ -6,7 +7,23 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            Console.WriteLine($"Hello, {args[0]}!");
+
+            var book = new Book("Mac Book");
+            book.AddGrade(1.5);
+
+            var grades = new List<double>() {1.5, 3.5, 7.5, 4.5};
+            grades.Add(2.5);
+
+            var result = 0.0;
+            foreach(var number in grades)
+            {
+                result += number;
+            }
+
+            result /= grades.Count;
+            Console.WriteLine($"The average grade is: {result:N1}");
+
+
         }
     }
 }
